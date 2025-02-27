d160f3853bd767531112cf5536bb31de3e87a1ec
redtrovelvet <harshita.h2810@gmail.com>
2025-02-27 16:06:45 -0700

    data exploration complete!

--- analysis.py
+++ analysis.py
@@ -16,23 +16,11 @@ df_test = pd.read_csv("hf://datasets/wwydmanski/wisconsin-breast-cancer/test.csv
 '''
 Data Cleaning
 '''
-# Dropping the first column (id column) from the train & test datasets
+# Removing unwanted columns (id) from the train & test datasets
 df_train.drop(columns=["Unnamed: 0"],inplace=True)
 df_test.drop(columns=["Unnamed: 0"],inplace=True)
 
-# Separating the diagnosis column (response) from the train & test datasets
-y_train = df_train['y']
-y_test = df_test['y']
-
-# Covnerting diagnosis labels to binary: B -> 0, M -> 1 
-y_train = y_train.map({'B': 0, 'M': 1})
-y_test = y_test.map({'B': 0, 'M': 1})
-
-# Dropping the diagnosis column from the train & test datasets
-X_train = df_train.drop(columns=['y'])
-X_test = df_test.drop(columns=['y'])
-
-# Renaming columns of the train & test datasets to be descriptive
+# Renaming columns of the train & test datasets to be more descriptive
 new_column_names = [
     'radius_mean', 'texture_mean', 'perimeter_mean', 'area_mean',
     'smoothness_mean', 'compactness_mean', 'concavity_mean',
@@ -42,29 +30,53 @@ new_column_names = [
     'concave_points_se', 'symmetry_se', 'fractal_dimension_se',
     'radius_worst', 'texture_worst', 'perimeter_worst', 'area_worst',
     'smoothness_worst', 'compactness_worst', 'concavity_worst',
-    'concave_points_worst', 'symmetry_worst', 'fractal_dimension_worst'
+    'concave_points_worst', 'symmetry_worst', 'fractal_dimension_worst','diagnosis'
 ]
-X_train.columns = new_column_names
-X_test.columns = new_column_names
+df_train.columns = new_column_names
+df_test.columns = new_column_names
+
+# Covnerting diagnosis labels to binary: B -> 0, M -> 1
+df_train['diagnosis'] = df_train['diagnosis'].map({'B': 0, 'M': 1})
+df_test['diagnosis'] = df_test['diagnosis'].map({'B': 0, 'M': 1})
 
 
 '''
 Data Exploration
 '''
+# Creating a DataFrame with predictors and response for exploration
+df_explore = df_train.copy()
 
+# Summary statistics for each feature grouped by diagnosis
+group_stats = df_explore.groupby('diagnosis').describe().T
+print(group_stats)
 
-
-
-
+# Heatmap of the correlation matrix of all features 
+corr_matrix = df_explore.corr()
+plt.figure(figsize=(15, 15))
+sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', fmt=".2f")
+plt.xticks(rotation=35, ha='right')
+plt.yticks(rotation=0)
+plt.title("Correlation Heatmap of All Features")
+plt.show()
 
 
 '''
 Preprocessing the train & test dataset
 '''
+# Separating the diagnosis column (response) from the train & test datasets
+y_train = df_train['diagnosis']
+y_test = df_test['diagnosis']
+
+# Removing the diagnosis column from the train & test datasets
+X_train = df_train.drop(columns=['diagnosis'])
+X_test = df_test.drop(columns=['diagnosis'])
+
 # Scaling the datasets since they contain continuous features
 scaler = StandardScaler()
+
 # Fitting the scaler on the train dataset and transforming it
 X_train_scaled = scaler.fit_transform(X_train)
+
 # Transforming the test dataset using the same scale as the train dataset to avoid data leakage
 X_test_scaled = scaler.transform(X_test)
 
