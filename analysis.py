8835f14e7701b518ab066b22225284d2ee9332f1
redtrovelvet <harshita.h2810@gmail.com>
2025-02-27 13:38:52 -0700

    adding data exploration

--- analysis.py
+++ analysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.preprocessing import StandardScaler
 import sklearn.model_selection as skm
 from sklearn.linear_model import LogisticRegression
@@ -13,21 +14,25 @@ df_test = pd.read_csv("hf://datasets/wwydmanski/wisconsin-breast-cancer/test.csv
 
 
 '''
-Preprocessing the test dataset
+Data Cleaning
 '''
-# Dropping the first column (id column) from the test dataset
+# Dropping the first column (id column) from the train & test datasets
+df_train.drop(columns=["Unnamed: 0"],inplace=True)
 df_test.drop(columns=["Unnamed: 0"],inplace=True)
 
-# Separating the diagnosis column (response) from the test dataset 
+# Separating the diagnosis column (response) from the train & test datasets
+y_train = df_train['y']
 y_test = df_test['y']
 
-# Covnerting diagnosis labels to binary: B -> 0, M -> 1
+# Covnerting diagnosis labels to binary: B -> 0, M -> 1 
+y_train = y_train.map({'B': 0, 'M': 1})
 y_test = y_test.map({'B': 0, 'M': 1})
 
-# Dropping the diagnosis column from the test dataset
+# Dropping the diagnosis column from the train & test datasets
+X_train = df_train.drop(columns=['y'])
 X_test = df_test.drop(columns=['y'])
 
-# Renaming columns to be descriptive
+# Renaming columns of the train & test datasets to be descriptive
 new_column_names = [
     'radius_mean', 'texture_mean', 'perimeter_mean', 'area_mean',
     'smoothness_mean', 'compactness_mean', 'concavity_mean',
@@ -39,24 +44,28 @@ new_column_names = [
     'smoothness_worst', 'compactness_worst', 'concavity_worst',
     'concave_points_worst', 'symmetry_worst', 'fractal_dimension_worst'
 ]
+X_train.columns = new_column_names
 X_test.columns = new_column_names
 
 
 '''
-Preprocessing the train dataset
+Data Exploration
 '''
-# Dropping the first column (id column) from the train dataset
-df_train.drop(columns=["Unnamed: 0"],inplace=True)
 
-# Separating the diagnosis column (response) from the train dataset 
-y_train = df_train['y']
 
-# Covnerting diagnosis labels to binary: B -> 0, M -> 1
-y_train = y_train.map({'B': 0, 'M': 1})
 
-# Dropping the diagnosis column from the train dataset
-X_train = df_train.drop(columns=['y'])
 
-# Renaming columns to be descriptive
-X_train.columns = new_column_names
+
+
+
+'''
+Preprocessing the train & test dataset
+'''
+# Scaling the datasets since they contain continuous features
+scaler = StandardScaler()
+# Fitting the scaler on the train dataset and transforming it
+X_train_scaled = scaler.fit_transform(X_train)
+# Transforming the test dataset using the same scale as the train dataset to avoid data leakage
+X_test_scaled = scaler.transform(X_test)
+
 
